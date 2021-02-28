6ef3f71e4516367046210c04d4657d992791a35f
Laura Silvanaviciute <laura.silvanaviciute@shopify.com>
2021-02-28 11:17:21 +0200

    feat: added dogiano instrument

--- src/components/Instrument.vue
+++ src/components/Instrument.vue
@@ -1,43 +1,196 @@
 <template>
   <div>
-    <v-btn
-      v-for="note of notes"
-      @click="playNote(note)"
-      v-bind:key="note"
-      small
-    >
-      ♪ {{ note }}
-    </v-btn>
+    <div>
+      <v-btn
+        v-for="note of notes"
+        @click="playNote(note)"
+        v-bind:key="note"
+        large
+        tile
+        text
+        outlined
+        color="teal"
+      >
+        ♪ {{ note }}
+      </v-btn>
+    </div>
+    <div>
+      <v-btn @click="toggleMelody()" large tile text outlined color="teal">
+        🎹 Melody
+      </v-btn>
+      <v-btn @click="toggleBass()" large tile text outlined color="teal">
+        🎸 Bass
+      </v-btn>
+      <v-btn @click="toggleNoise()" large tile text outlined color="teal">
+        💨 Noise
+      </v-btn>
+      <v-btn
+        @click="playRandomMarioSample()"
+        large
+        tile
+        text
+        outlined
+        color="teal"
+      >
+        🍄 Mario
+      </v-btn>
+      <v-btn @click="speedUpBpm()" large tile text outlined color="teal">
+        👟 Speed up
+      </v-btn>
+      <v-btn @click="stopInstruments()" large tile text outlined color="teal">
+        ✋ Stop
+      </v-btn>
+    </div>
   </div>
 </template>
 
 <script>
 import * as Tone from "tone";
-import { GAMMA_NOTES } from "../constants/notes";
-import CatSample from "../assets/cat-sample.mp3";
+import {
+  DOGIANO_GAMMA_NOTES,
+  MELODY_CHORDS,
+  BASS_CHORDS,
+  MARIO_SAMPLES,
+} from "../constants/notes";
+import DogSample from "../assets/dog-sample.mp3";
 
 export default {
   name: "Instrument",
   data() {
     return {
-      synth: null,
-      notes: GAMMA_NOTES,
+      polySynthSquare: null,
+      polySynthSaw: null,
+      notes: DOGIANO_GAMMA_NOTES,
       volume: null,
       sampler: null,
+      mediumVolume: null,
+      melodyPart: null,
+      bassPart: null,
+      noise: null,
+      isTransportStarted: null,
     };
   },
   created() {
-    this.synth = new Tone.Synth().toDestination();
-
-    this.volume = new Tone.Volume(-5);
-    this.sampler = new Tone.Sampler({
-      G2: CatSample,
-    }).chain(this.volume, Tone.Destination);
+    this.mediumVolume = new Tone.Volume(-15);
+    this.initDogianoInstrument();
   },
   methods: {
+    initDogianoInstrument() {
+      this.sampler = new Tone.Sampler({
+        E4: DogSample,
+      }).chain(this.mediumVolume, Tone.Destination);
+
+      this.polySynthSquare = new Tone.PolySynth(Tone.Synth, {
+        oscillator: {
+          type: "square",
+        },
+      }).chain(new Tone.Volume(-25), Tone.Destination);
+    },
+
     playNote(note) {
-      this.synth.triggerAttackRelease(note, "4n");
+      this.polySynthSquare.triggerAttackRelease(note, "4n");
       this.sampler.triggerAttack(note);
+    },
+
+    toggleMelody() {
+      if (this.melodyPart) {
+        this.melodyPart.mute = !this.melodyPart.mute;
+      } else {
+        this.polySynthSaw = new Tone.PolySynth(Tone.Synth, {
+          oscillator: {
+            type: "fatsawtooth",
+          },
+          envelope: {
+            attack: 0.01,
+            release: 0.4,
+          },
+        }).chain(this.mediumVolume, Tone.Destination);
+
+        this.melodyPart = new Tone.Part((time, chord) => {
+          this.sampler.triggerAttackRelease(chord.note, chord.duration, time);
+          this.polySynthSaw.triggerAttackRelease(
+            chord.note,
+            chord.duration,
+            time
+          );
+        }, MELODY_CHORDS);
+
+        this.playPart(this.melodyPart);
+      }
+    },
+
+    playPart(part) {
+      if (!this.isTransportStarted) {
+        Tone.Transport.toggle();
+        this.isTransportStarted = true;
+        Tone.Transport.bpm.value = 132;
+      }
+
+      part.start(0);
+      part.loop = true;
+      part.loopEnd = "4m";
+    },
+
+    toggleBass() {
+      if (this.bassPart) {
+        this.bassPart.mute = !this.bassPart.mute;
+      } else {
+        this.bassPart = new Tone.Part((time, chord) => {
+          this.polySynthSquare.triggerAttackRelease(
+            chord.note,
+            chord.duration,
+            time
+          );
+        }, BASS_CHORDS);
+
+        this.playPart(this.bassPart);
+      }
+    },
+
+    toggleNoise() {
+      if (this.noise) {
+        this.noise.mute = !this.noise.mute;
+      } else {
+        this.noise = new Tone.Noise("pink").start();
+        const filter = new Tone.AutoFilter({
+          frequency: "8m",
+        }).chain(new Tone.Volume(-20), Tone.Destination);
+
+        this.noise.connect(filter);
+        filter.start();
+      }
+    },
+
+    playRandomMarioSample() {
+      this.playSample(
+        MARIO_SAMPLES[Math.floor(Math.random() * MARIO_SAMPLES.length)]
+      );
+    },
+
+    playSample(sampleName) {
+      new Tone.Player({
+        url: sampleName,
+        autostart: true,
+      }).chain(this.mediumVolume, Tone.Destination);
+    },
+
+    speedUpBpm() {
+      Tone.Transport.bpm.value += 20;
+    },
+
+    stopInstruments() {
+      this.playSample(MARIO_SAMPLES[5]);
+
+      this.stopInstrument(this.melodyPart);
+      this.stopInstrument(this.bassPart);
+      this.stopInstrument(this.noise);
+    },
+
+    stopInstrument(instrument) {
+      if (instrument) {
+        instrument.mute = true;
+        instrument.stop();
+      }
     },
   },
 };
--- src/constants/notes.js
+++ src/constants/notes.js
@@ -1,17 +1,247 @@
+import BumpSample from "../assets/bump.mp3";
+import CoinSample from "../assets/coin.mp3";
+import FireballSample from "../assets/fireball.mp3";
+import FlagpoleSample from "../assets/flagpole.mp3";
+import JumpSample from "../assets/jump.mp3";
+import PowerupSample from "../assets/powerup.mp3";
+
 export const GAMMA_NOTES = [
-    "C2",
-    "D2",
-    "E2",
-    "F2",
-    "G2",
-    "A2",
-    "B2",
-    "C3",
-    "D3",
-    "E3",
-    "F3",
-    "G3",
-    "A3",
-    "B3",
-    "C4",
+  "C2",
+  "D2",
+  "E2",
+  "F2",
+  "G2",
+  "A2",
+  "B2",
+  "C3",
+  "D3",
+  "E3",
+  "F3",
+  "G3",
+  "A3",
+  "B3",
+  "C4",
+];
+
+export const MELODY_CHORDS = [
+  {
+    time: "192i",
+    note: "G4",
+    duration: "104i",
+  },
+  {
+    time: "192i",
+    note: "B4",
+    duration: "104i",
+  },
+  {
+    time: "192i",
+    note: "D5",
+    duration: "104i",
+  },
+  {
+    time: "480i",
+    note: "G4",
+    duration: "104i",
+  },
+  {
+    time: "480i",
+    note: "C5",
+    duration: "104i",
+  },
+  {
+    time: "480i",
+    note: "E5",
+    duration: "104i",
+  },
+  {
+    time: "768i",
+    note: "F4",
+    duration: "104i",
+  },
+  {
+    time: "768i",
+    note: "A4",
+    duration: "104i",
+  },
+  {
+    time: "768i",
+    note: "C5",
+    duration: "104i",
+  },
+  {
+    time: "1056i",
+    note: "F4",
+    duration: "104i",
+  },
+  {
+    time: "1056i",
+    note: "A4",
+    duration: "104i",
+  },
+  {
+    time: "1056i",
+    note: "C5",
+    duration: "104i",
+  },
+  {
+    time: "1248i",
+    note: "G4",
+    duration: "104i",
+  },
+  {
+    time: "1248i",
+    note: "B4",
+    duration: "104i",
+  },
+  {
+    time: "1248i",
+    note: "D5",
+    duration: "104i",
+  },
+  {
+    time: "1440i",
+    note: "G4",
+    duration: "248i",
+  },
+  {
+    time: "1440i",
+    note: "B4",
+    duration: "248i",
+  },
+  {
+    time: "1440i",
+    note: "D5",
+    duration: "248i",
+  },
+  {
+    time: "1728i",
+    note: "G4",
+    duration: "104i",
+  },
+  {
+    time: "1728i",
+    note: "C5",
+    duration: "104i",
+  },
+  {
+    time: "1728i",
+    note: "E5",
+    duration: "104i",
+  },
+  {
+    time: "2016i",
+    note: "F4",
+    duration: "104i",
+  },
+  {
+    time: "2016i",
+    note: "A4",
+    duration: "104i",
+  },
+  {
+    time: "2016i",
+    note: "C5",
+    duration: "104i",
+  },
+  {
+    time: "2208i",
+    note: "C4",
+    duration: "296i",
+  },
+  {
+    time: "2208i",
+    note: "F4",
+    duration: "200i",
+  },
+  {
+    time: "2208i",
+    note: "A4",
+    duration: "208i",
+  },
+  {
+    time: "2400i",
+    note: "E4",
+    duration: "104i",
+  },
+  {
+    time: "2400i",
+    note: "G4",
+    duration: "104i",
+  },
+  {
+    time: "2592i",
+    note: "C4",
+    duration: "488i",
+  },
+  {
+    time: "2592i",
+    note: "D4",
+    duration: "488i",
+  },
+  {
+    time: "2592i",
+    note: "G4",
+    duration: "488i",
+  },
+];
+
+export const BASS_CHORDS = [
+  {
+    time: 0,
+    note: "C2",
+    duration: "768i",
+  },
+  {
+    time: "768i",
+    note: "C2",
+    duration: "768i",
+  },
+  {
+    time: "1536i",
+    note: "C2",
+    duration: "768i",
+  },
+  {
+    time: "2304i",
+    note: "C2",
+    duration: "192i",
+  },
+  {
+    time: "2496i",
+    note: "F1",
+    duration: "192i",
+  },
+  {
+    time: "2688i",
+    note: "G1",
+    duration: "384i",
+  },
+];
+
+export const DOGIANO_GAMMA_NOTES = [
+  "C3",
+  "D3",
+  "E3",
+  "F3",
+  "G3",
+  "A3",
+  "B3",
+  "C4",
+  "D4",
+  "E4",
+  "F4",
+  "G4",
+  "A4",
+  "B4",
+  "C5",
+];
+
+export const MARIO_SAMPLES = [
+  BumpSample,
+  CoinSample,
+  FireballSample,
+  FlagpoleSample,
+  JumpSample,
+  PowerupSample,
 ];
